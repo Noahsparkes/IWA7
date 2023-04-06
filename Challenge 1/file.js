3571167979d539a1d519097dd0296ff7c7348abe
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-06 09:15:53 +0200

    final changes to challenge 1

--- Challenge 1/file.js
+++ Challenge 1/file.js
@@ -1,5 +1,8 @@
-const value = "3"
-console.log(value + 4 + value)
+const value = 3;
+const number = 4;
+let result = (value + number + value) 
+
+console.log(result);
 
 /*'value' is initially stated to have a value of 3 
 when it is console logged it is added to 4, however these values do not mathematically add together instead they concatinate.*/
