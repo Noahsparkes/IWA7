fd49adb32e555de966bbf1ead7b57cea7b6838d6
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-06 09:35:11 +0200

    explanation wy it didn't work.

--- Challenge 2/file2.js
+++ Challenge 2/file2.js
@@ -1,6 +1,7 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
 
-console.log(`Good Morning, ${nickname} ${firstname}!`)
+console.log(`Good Morning, ${nickname}, ${firstname}!`)
 
-//should log a message that says Good morning with a dynamically entered name (from two possible variables).
+/*was originally not working because this operation does not work in console.log without (``) backticks, 
+the second reason Firstname wasn't displaying was because of the missing '$' that puts this logical operation together.*/
