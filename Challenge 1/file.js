cfc3a4dabf48d97641d64223bddcf651fd18ac21
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-06 09:05:13 +0200

    explanation- why the console log has this answer

--- Challenge 1/file.js
+++ Challenge 1/file.js
@@ -1,2 +1,5 @@
 const value = "3"
 console.log(value + 4 + value)
+
+/*'value' is initially stated to have a value of 3 
+when it is console logged it is added to 4, however these values do not mathematically add together instead they concatinate.*/
