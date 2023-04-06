0128c81ac4f6193c1750d96b1a99d3d7f75d2bf6
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-06 09:26:48 +0200

    backticks added instead of quotation marks

--- Challenge 2/file2.js
+++ Challenge 2/file2.js
@@ -1,4 +1,6 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
 
-console.log("Good Morning, ${nickname} || {firstname}!")
+console.log(`Good Morning, ${nickname} || {firstname}!`)
+
+//should log a message that says Good morning with a dynamically entered name (from two possible variables).
