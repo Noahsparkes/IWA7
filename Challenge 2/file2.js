a59a6a880daea66cb17c38b8714ac3e85db7e5ec
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-06 09:35:42 +0200

    spelling change

--- Challenge 2/file2.js
+++ Challenge 2/file2.js
@@ -4,4 +4,4 @@ const firstname = "Timothy";
 console.log(`Good Morning, ${nickname}, ${firstname}!`)
 
 /*was originally not working because this operation does not work in console.log without (``) backticks, 
-the second reason Firstname wasn't displaying was because of the missing '$' that puts this logical operation together.*/
+the second reason Firstname wasn't displaying was because of the missing '$' that puts this logical operator together.*/
