848a8f248237b29e7b5705ea3014e09785c28395
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-07 12:50:38 +0200

    final changes to challenge 2

--- Challenge 2/file2.js
+++ Challenge 2/file2.js
@@ -1,14 +1,14 @@
-const nickname= "Timmy";
+const nickname = "Timmy";
 const firstname = "Timothy";
 
-//console.log(`Good Morning, ${nickname} || ${firstname}!`)
-
-if (nickname){
-    console.log(`Good Morning, ${nickname}!`);
+if (nickname) {
+  console.log(`Good Morning, ${nickname}!`);
 } else if (firstname) {
-    console.log(`Good Morning, ${firstname}!`);
+  console.log(`Good Morning, ${firstname}!`);
 } else {
-    console.log("Good Morning")
+  console.log("Good Morning");
+
+  console.log(`Good Morning, ${nickname} || ${firstname}!`) //corrected console.log statement
 }
 
 /*Was originally not working because this operation does not work in console.log without (``) backticks, 
