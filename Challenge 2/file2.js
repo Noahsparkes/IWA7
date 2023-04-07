d70b4965ebd5b0365efbc24922ca5d062b649731
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-07 12:38:42 +0200

    seting up if statements

--- Challenge 2/file2.js
+++ Challenge 2/file2.js
@@ -1,7 +1,20 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
 
-console.log(`Good Morning, ${nickname}, ${firstname}!`)
+//console.log(`Good Morning, ${nickname} || ${firstname}!`)
 
-/*was originally not working because this operation does not work in console.log without (``) backticks, 
+/*Was originally not working because this operation does not work in console.log without (``) backticks, 
 the second reason Firstname wasn't displaying was because of the missing '$' that puts this logical operator together.*/
+
+
+if (nickname){
+    console.log(`Good Morning, ${nickname}!`)
+} else {
+    console.log('Good Morning')
+}
+
+if (firstname){
+    console.log(`Good Morning, ${firstname}!`)
+} else {
+    console.log('Good Morning')
+}
