3fab648885101d2d70e8ba0b9e5330e1b1edbd17
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-07 12:45:00 +0200

    fixed if statement with else added.

--- Challenge 2/file2.js
+++ Challenge 2/file2.js
@@ -3,18 +3,13 @@ const firstname = "Timothy";
 
 //console.log(`Good Morning, ${nickname} || ${firstname}!`)
 
+if (nickname){
+    console.log(`Good Morning, ${nickname}!`);
+} else if (firstname) {
+    console.log(`Good Morning, ${firstname}!`);
+} else {
+    console.log("Good Morning")
+}
+
 /*Was originally not working because this operation does not work in console.log without (``) backticks, 
 the second reason Firstname wasn't displaying was because of the missing '$' that puts this logical operator together.*/
-
-
-if (nickname){
-    console.log(`Good Morning, ${nickname}!`)
-} else {
-    console.log('Good Morning')
-}
-
-if (firstname){
-    console.log(`Good Morning, ${firstname}!`)
-} else {
-    console.log('Good Morning')
-}
