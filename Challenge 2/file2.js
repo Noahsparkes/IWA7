620025ffc80d1208248a805e2fb000c96ba5eb22
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-08 09:41:54 +0200

    changes made to Challenge 2&3

--- Challenge 2/file2.js
+++ Challenge 2/file2.js
@@ -7,9 +7,18 @@ if (nickname) {
   console.log(`Good Morning, ${firstname}!`);
 } else {
   console.log("Good Morning");
-
-  console.log(`Good Morning, ${nickname} || ${firstname}!`) //corrected console.log statement
 }
+
+/*alt answer
+if (`!${firstname} || !${nickname}`){
+  console.log(firstname || nickname);
+} else {
+ console.log('Good Morning')
+}
+*/
 
 /*Was originally not working because this operation does not work in console.log without (``) backticks, 
 the second reason Firstname wasn't displaying was because of the missing '$' that puts this logical operator together.*/
+
+
+
--- Challenge 3/file3.js
+++ Challenge 3/file3.js
@@ -1,19 +1,39 @@
-const leoName = 'Leo'
-const leoSurname = 'Musvaire     '
-const leoBalance = '-9394'
 
-const sarahName = 'Sarah    '
-const sarahSurname = 'Kleinhans'
-const sarahBalance = '-4582.21000111'
+const leoName = 'Leo';
+const leoSurname = 'Musvaire     ';
+const leoBalance = '-9394';
 
-const divider = '----------------------------------'
+const sarahName = 'Sarah    ';
+const sarahSurname = 'Kleinhans';
+const sarahBalance = '-4582.21000111';
 
-// Only change below this line
+const divider = '----------------------------------\n';
 
-const owed = parseInt('R' + leoBalance + sarahBalance)
-const leo = "{leoName} + {leoSurname} + \"Owed\" + \"R\" + {sarahBalance}"
-const sarah = "{leoName} + {surname} + \"Owed\" + \"R\" + {sarahBalance}"
-const total = "Total amount owed: "
-const result = leo + sarah + divider + divider + total + owed + divider
+//Only change code below this line
 
-console.log(result)
+const owed = parseFloat(leoBalance) + parseFloat(sarahBalance);
+const leo = `${leoName} ${leoSurname} Owed R ${parseFloat(leoBalance).toFixed(2)}\n`;
+const sarah = `${sarahName} ${sarahSurname} Owed R ${parseFloat(sarahBalance).toFixed(2)}\n`;
+const total = `  Total amount owed: R ${Math.abs(owed).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}\n`;
+const result = '\n' + leo + sarah + divider + total + divider;
+
+console.log(result);
+
+
+/* expected answer when console.logged:
+
+Leo Musvaire (Owed: R 9394.00)
+Sarah Kleinhans (Owed: R 4582.20)
+
+----------------------------------
+  Total amount owed: R 14 976.20
+----------------------------------*/
+
+
+
+
+
+
+
+/*const num = -7
+console.log(Math.abs(num))*/
